088b9efc3d168f920e66f98a6ebd5ce986d666bd
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-04-02 01:05:05 +0200

    update

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
-platform :ios, '8.0'
+platform :tvos, '8.0'
 
 use_frameworks!
 
 pod 'OHPDFImage'
+pod 'SnapKit'
--- slides/UIPDFController.swift
+++ slides/UIPDFController.swift
@@ -7,37 +7,132 @@
 //
 
 import Foundation
+import SnapKit
 
 class UIPDFPageCell : UICollectionViewCell {
+    
+    lazy var box:UIImageView = UIImageView()
+    
+    
+    private func initialize() {
+    
+        self.addSubview(box)
+        
+         box.snp_makeConstraints { (make) -> Void in
+            make.width.height.equalTo(self)
+            make.top.equalTo(self)
+            make.bottom.equalTo(self)
+         }
+        
+        self.layoutIfNeeded()
+        self.layoutSubviews()
+        self.setNeedsDisplay()
+        
+        
+        //self.box.adjustsImageWhenAncestorFocused = true
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initialize()
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        initialize()
+        
+    }
+   
+    override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+        if (self.focused)
+        {
+            self.box.adjustsImageWhenAncestorFocused = true
+        }
+        else
+        {
+            self.box.adjustsImageWhenAncestorFocused = false
+        }
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    
     
 }
 
 
-class UIPDFCollectionViewController : UICollectionViewController {
+class UIPDFCollectionViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout {
  
     var doc:OHPDFDocument!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         let path = NSBundle.mainBundle().pathForResource("rx1", ofType: "pdf")
         
         let url = NSURL(fileURLWithPath: path!)
         
         doc = OHPDFDocument(URL: url)
         
+        self.view.setNeedsFocusUpdate()
+        self.view.updateFocusIfNeeded()
         
+    }
+    
+// MARK: <UICollectionViewDelegateFlowLayout>
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
+    {
+        //print("sizeForItemAtIndexPath")
+        return CGSizeMake(180,260) //use height whatever you wants.
+    }
+    
+    //func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets;
+    //func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize
+    //func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat
+    //func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat
+    //func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize
+    
+    
+// MARK: <UICollectionViewDelegate>
+    
+    override internal func collectionView(collectionView: UICollectionView, canFocusItemAtIndexPath indexPath: NSIndexPath) -> Bool
+    {
+        print( "canFocusItemAtIndexPath(\(indexPath.row))" )
+        return true
     }
     
 // MARK: <UICollectionViewDataSource>
     
     override internal func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //print( "page # \(doc.pagesCount)")
         return doc.pagesCount
     }
     
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     override internal func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("slide", forIndexPath:indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("slide", forIndexPath:indexPath) as! UIPDFPageCell
         
+        let page = doc.pageAtIndex(indexPath.row+1)
+        
+        let vectorImage = OHVectorImage(PDFPage: page)
+        
+        cell.box.image = vectorImage.renderAtSize(cell.frame.size)
         
         return cell;
 
