4fc67e2b0e54b9f36ffd610092dff9b85ed7c933
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-02-13 02:37:25 +0100

    eremove from commit

--- Pods/Target Support Files/SettingsKit/SettingsKit-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SettingsKit.h"
-
-FOUNDATION_EXPORT double SettingsKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char SettingsKitVersionString[];
-
