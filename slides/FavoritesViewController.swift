b32e76ea03fba4f22ab0ef9ca5cd5591229dd951
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-06-14 11:48:01 +0200

    issue #4 - show favourites table

--- slides/FavoritesViewController.swift
+++ slides/FavoritesViewController.swift
@@ -7,28 +7,60 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
 
+    let disposeBag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        /*
+        tableView.rx
+            .modelSelected(String.self)
+            .subscribe(onNext:  { value in
+            })
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .itemAccessoryButtonTapped
+            .subscribe(onNext: { indexPath in
+            })
+            .disposed(by: disposeBag)
+        */
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    private var bindTo:Disposable?
+    
     override func viewWillAppear(_ animated: Bool) {
+        let favoriteItems = rxFavorites().toArray()
         
-        let _ = rxFavorites()
-            .subscribe( onNext: { (pair:(key: String, value: Any)) in
-                print( "VALUE \(pair)")
-            })
-        
+        bindTo = favoriteItems
+            .bind(to: tableView.rx.items(cellIdentifier: "favoriteCell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                
+                if let data = element.value as? [String:String], let title = data["title"] {
+                    
+                    cell.textLabel?.textAlignment = .center
+                    cell.textLabel?.text = "\(title) @ row \(row)"
+                    
+                }
+            }
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        bindTo?.dispose()
+    }
+    
     /*
     // MARK: - Navigation
 
