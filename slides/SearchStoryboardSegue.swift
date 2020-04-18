f993b9dfcb98e43ff6890af8872556ba4604428d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-04-18 16:16:23 +0200

    set searchController background

--- slides/SearchStoryboardSegue.swift
+++ slides/SearchStoryboardSegue.swift
@@ -35,30 +35,36 @@ class SearchStoryboardSegue: UIStoryboardSegue {
     
     func packagedSearchController( searchResultsController:SearchSlidesViewController ) -> UISearchContainerViewController {
         
-        if let w = searchResultsController.collectionView {
-            
-            //var f = w.frame.insetBy(dx: 90, dy: 60 )
-            var ff = w.frame
-            
-            ff.origin.x     = ff.origin.x + 90
-            ff.size.width   = ff.size.width - 180
-            ff.size.height  = ff.size.height - 60
-            
-            w.frame = ff
-            
-        }
+//        if let wiew = searchResultsController.collectionView {
+//            
+//            //var f = w.frame.insetBy(dx: 90, dy: 60 )
+//            var ff = wiew.frame
+//            
+//            ff.origin.x     = ff.origin.x + 90
+//            ff.size.width   = ff.size.width - 180
+//            ff.size.height  = ff.size.height - 60
+//            
+//            wiew.frame = ff
+//            
+//        }
 
         let searchController = UISearchController(searchResultsController: searchResultsController)
-        
-        searchController.searchResultsUpdater = searchResultsController
-        searchController.searchBar.placeholder = NSLocalizedString("what", comment: "")
-        
         // Contain the `UISearchController` in a `UISearchContainerViewController`.
+
+        searchController.searchResultsUpdater = searchResultsController
+        searchController.searchBar.placeholder = "Search for slides" //NSLocalizedString("what", comment: "")
+        
+        // Apperance
+        let bgColor = searchResultsController.collectionView.backgroundColor
+        searchResultsController.view.backgroundColor = bgColor
+        searchController.view.backgroundColor = bgColor
+        searchController.searchBar.backgroundColor = .white
+
         let searchContainer = UISearchContainerViewController(searchController: searchController)
         //searchContainer.title = NSLocalizedString("Search for slides", comment: "")
-        
+
         return searchContainer
         
     }
-    
+ 
 }
