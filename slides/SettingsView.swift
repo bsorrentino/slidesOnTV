02d663633f4082f83d86af0965d8919e6d2dd3cc
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-04-18 17:15:49 +0200

    update settings view appearance

--- slides/SettingsView.swift
+++ slides/SettingsView.swift
@@ -25,28 +25,43 @@ class SettingButton : UIButton, NameDescribable {
 //
 //
 //    }
-
+    
     override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         print( "\(typeName).didUpdateFocusInContext: focused:" );
 
         super.didUpdateFocus(in: context, with: coordinator)
         
         coordinator.addCoordinatedAnimations(nil) {
-          // Perform some task after item has received focus
+            // Perform some task after item has received focus
             if context.nextFocusedView == self {
                 
-                //self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
-                self.layer.shadowColor      = UIColor.black.withAlphaComponent(0.50).cgColor
-                self.layer.shadowOffset     = CGSize(width: 0.0, height: 3.0)
-                self.layer.shadowOpacity    = 1.0
-                self.layer.shadowRadius     = 0.0
-                self.layer.masksToBounds    = false
-                self.layer.cornerRadius     = 4.0
             }
             else {
-                self.layer.shadowColor = UIColor.clear.cgColor
             }
         }
     }
+
+//    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+//        print( "\(typeName).didUpdateFocusInContext: focused:" );
+//
+//        super.didUpdateFocus(in: context, with: coordinator)
+//
+//        coordinator.addCoordinatedAnimations(nil) {
+//          // Perform some task after item has received focus
+//            if context.nextFocusedView == self {
+//
+//                //self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+//                self.layer.shadowColor      = UIColor.black.withAlphaComponent(0.50).cgColor
+//                self.layer.shadowOffset     = CGSize(width: 0.0, height: 3.0)
+//                self.layer.shadowOpacity    = 1.0
+//                self.layer.shadowRadius     = 0.0
+//                self.layer.masksToBounds    = false
+//                self.layer.cornerRadius     = 4.0
+//            }
+//            else {
+//                self.layer.shadowColor = UIColor.clear.cgColor
+//            }
+//        }
+//    }
 
 }
