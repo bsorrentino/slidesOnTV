21596b00c70a0b70de8bca65fd03e5e5680ba992
Build Pipeline <build.pipeline@softphone.it>
2021-07-02 16:49:22 +0200

    add first screen, searchBar

---
+++ slides/slides/SearchBar.swift
@@ -0,0 +1,9 @@
+//
+//  SearchBar.swift
+//  slides
+//
+//  Created by Bartolomeo Sorrentino on 02/07/21.
+//  Copyright © 2021 bsorrentino. All rights reserved.
+//
+
+import Foundation
---
+++ slides/slides/SearchSlides.swift
@@ -0,0 +1,21 @@
+//
+//  SearchSlides.swift
+//  slides
+//
+//  Created by Bartolomeo Sorrentino on 02/07/21.
+//  Copyright © 2021 bsorrentino. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchSlides: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchSlides_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchSlides()
+    }
+}
