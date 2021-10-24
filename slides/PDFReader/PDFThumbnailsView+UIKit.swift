1da5febc5022979d894aaca2243b273a5f05d1ef
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-10-24 23:13:41 +0200

    update(code)

--- slides/PDFReader/PDFThumbnailsView+UIKit.swift
+++ slides/PDFReader/PDFThumbnailsView+UIKit.swift
@@ -8,33 +8,75 @@
 
 import SwiftUI
 
+struct PDFThumbnailsView : UIViewControllerRepresentable {
+        
+    typealias UIViewControllerType = PDFThumbnailsViewController
+    
+    class Coordinator: NSObject, UITableViewDelegate {
+        
+    }
+    
+    var document : PDFDocument
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<PDFThumbnailsView>) -> UIViewControllerType {
+        
+        let controller = PDFThumbnailsViewController( document: document )
+        
+        controller.tableView.delegate = context.coordinator
+        
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<PDFThumbnailsView>) {
+    
+    }
+    
+}
 
-public class PDFThumbnailsView : UIViewController  {
+
+
+
+public class PDFThumbnailsViewController : UIViewController  {
+        
+    var document:PDFDocument
     
     fileprivate var tableView: UITableView! {
        didSet {
-         tableView.delegate = self
          tableView.dataSource = self
-         tableView.tableFooterView = UIView(frame: CGRect.zero)
-         tableView.backgroundColor = .black
+         //tableView.delegate = self
+         // tableView.tableFooterView = UIView(frame: CGRect.zero)
+         // tableView.backgroundColor = .black
          view.addSubview(tableView)
        }
     }
+    
+    internal init(document: PDFDocument) {
+        self.document = document
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
 }
 
-// MARK: PDFThumbnailsView (UITableViewDelegate)
-extension PDFThumbnailsView : UITableViewDelegate {
-    
-}
 
 // MARK: PDFThumbnailsView (UITableViewDataSource)
-extension PDFThumbnailsView : UITableViewDataSource {
+extension PDFThumbnailsViewController : UITableViewDataSource {
+    
     public func numberOfSections(in tableView: UITableView) -> Int {
       return 1
     }
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return document.pageCount
     }
     
     
