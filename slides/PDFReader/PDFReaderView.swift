d4773ad9930ef08170eb3c19a97f0e96477d9686
Build Pipeline <build.pipeline@softphone.it>
2021-11-01 18:33:10 +0100

    refactor(code): new PDFThumbnailsView

--- slides/PDFReader/PDFReaderView.swift
+++ slides/PDFReader/PDFReaderView.swift
@@ -7,52 +7,6 @@
 //
 import SwiftUI
 
-struct ThumbnailShadow: ViewModifier {
-    
-    var focused:Bool
-    
-    func body(content: Content) -> some View {
-        if (focused) {
-            return content
-                .shadow( color: Color.black, radius: 20, x:5, y:5 )
-        }
-        
-        return content.shadow( color: Color.clear, radius: 0 )
-    }
-}
-
-struct ThumbnailView : View {
-    //    @Environment(\.isFocused) var focused: Bool
-    
-    var uiImage:UIImage
-    var size:CGSize
-    
-    var body: some View {
-        
-        Image( uiImage: uiImage )
-            .resizable()
-            .frame(width: size.width,
-                   height: size.height,
-                   alignment: .center)
-    }
-}
-
-func pageNumberLabel( of pageNumber:Int ) -> some View {
-    
-    Circle()
-        .fill( Color.blue )
-        .shadow( color: Color.black, radius: 1, x:1, y:1)
-        .shadow( color: Color.black, radius: 1, x:1, y:1)
-        .frame(width: 30, height: 30)
-        .padding( EdgeInsets(top:0,leading:0,bottom:5,trailing:5))
-        .overlay(
-            Text("\(pageNumber)")
-                .padding( EdgeInsets(top:0,leading:0,bottom:5,trailing:5) )
-                .font(.system(size: 12).weight(.heavy))
-        )
-}
-
-
 
 struct PDFReaderContentView: View {
     @Namespace private var focusNS
@@ -70,26 +24,12 @@ struct PDFReaderContentView: View {
                 
                 if( !isZoom ) {
                     
-                    List( document.allPageNumbers, id: \.self ) { pageNumber in
-                        
-                        ThumbnailView( uiImage:document.pdfPageImage(at: pageNumber)!,
-                                       size:CGSize(width: geom.size.width * 0.2, height: geom.size.height * 0.25))
-                            .modifier( ThumbnailShadow( focused: pageSelected == pageNumber ) )
-                            .overlay( pageNumberLabel(of: pageNumber ), alignment: .bottomTrailing )
-                            .focusable(true) { focused in
-                                print( "thumbnail focus on page \(pageNumber) - focused:\(focused) - pinter:\(isPointerVisible)")
-                                if( focused && pageNumber != self.pageSelected ) {
-                                    self.pageSelected = pageNumber
-                                }
-                            }
-//                            .if( pageNumber == pageSelected ) {
-//                                $0.prefersDefaultFocus(in: focusNS )
-//                            }
-                    }
-                    .frame( width: geom.size.width * 0.2, height: geom.size.height - 1)
+                    //PDFThumbnailsView( document:document, pageSelected:$pageSelected, parentSize:geom.size )
+                    PDFThumbnailsViewUIKit( document:document, pageSelected:$pageSelected, parentSize:geom.size )
                     //  .focusable( isPointerVisible ) { focused in
                     //      print( "thumbnail List focus on page \(pageSelected) - focused:\(focused pointer:\(isPointerVisible)")
                     //   }
+                    .frame( width: geom.size.width * 0.2, height: geom.size.height - 1)
                     .prefersDefaultFocus(in: focusNS )
                 }
 //                if self.pageSelected > 0  {
@@ -101,9 +41,9 @@ struct PDFReaderContentView: View {
                             document:self.document,
                             pageSelected:self.pageSelected,
                             isPointerVisible:self.$isPointerVisible)
-                        .if( isZoom ) { view in
-                            view.prefersDefaultFocus(in: focusNS )
-                        }
+//                        .if( isZoom ) { view in
+//                            view.prefersDefaultFocus(in: focusNS )
+//                        }
                         
                         if( !isZoom ) { Spacer() }
                     }
