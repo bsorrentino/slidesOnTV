b22c3881419daffdb7dac0bff5418a6b9c860e0c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-12-14 20:07:53 +0100

    refactor: clean code

--- slides/slides/ContentView.swift
+++ slides/slides/ContentView.swift
@@ -9,21 +9,7 @@
 import SwiftUI
 
 
-func MainButton( text:String, image:String, action: @escaping () -> Void ) -> some View {
-
-    Button( action: action ) {
-            HStack {
-                Image(systemName: image)
-                Text(text)
-            }
-            .font(.title3)
-            .padding(30.0)
-    }
-    .buttonStyle(CardButtonStyle())
-
-}
-
-func NavigationButton<Destination :View>( text:String, image:String, destination: Destination ) -> some View {
+func NavigationButton<Destination :View>( text:String, image:String, destination: () -> Destination ) -> some View {
     NavigationLink( destination: destination ) {
             HStack {
                 Image(systemName: image)
@@ -40,20 +26,6 @@ struct ContentView: View {
     @State var searchText:String = ""
 
     
-    var Buttons:some View  {
-            VStack(alignment: .center, spacing:50) {
-
-                Spacer()
-                NavigationButton( text: "Search   ",
-                                  image: "magnifyingglass.circle.fill",
-                                  destination:SearchSlidesView() )
-                NavigationButton( text: "Favorites",
-                                  image:"bookmark.fill",
-                                  destination: FavoritesView() )
-                Spacer()
-            }
-    }
-    
     var BackgroundImage:some View {
         Image("tv-menu").resizable().frame( maxWidth: 1048)
     }
@@ -63,7 +35,21 @@ struct ContentView: View {
                
             HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
                 Spacer()
-                Buttons
+                VStack(alignment: .center, spacing:50) {
+                    Spacer()
+
+                    NavigationButton( text: "Search   ",
+                                      image: "magnifyingglass.circle.fill" )
+                    {
+                        SearchSlidesView()
+                    }
+                    NavigationButton( text: "Favorites",
+                                      image:"bookmark.fill" )
+                    {
+                        FavoritesView()
+                    }
+                    Spacer()
+                }
                 Spacer()
             }
             .background(BackgroundImage)
